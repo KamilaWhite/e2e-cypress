368390c1ddee8a2dea5daf54f02d0332f3865fea
Kamila <>
2021-05-04 22:29:46 +0200

    Pagination test edit

--- cypress/integration/TXM/Paginantion-test.spec.js
+++ cypress/integration/TXM/Paginantion-test.spec.js
@@ -3,18 +3,26 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 })
 
 describe("Chceck pagination tast cases" , () => {
-
-    it("Check pagination", () => {
+    beforeEach(() => {
         cy.visit('https://txm.pl/mezczyzna/bielizna-meska/skarpetki');
-        cy.get('.m-pagination_next').eq(0).click(); //top arrow
+      });
+
+    it("Check top arrow", () => {
+        cy.get('.m-pagination_next').eq(0).click();
         cy.get('.m-pagination_input').should('have.value', 2);
         cy.get('.m-pagination_prev').eq(0).click();
         cy.get('.m-pagination_input').should('have.value', 1);
-        cy.get('.m-pagination_next').eq(1).click(); //bottom arrow
+    });
+        
+    it("Check bottom arrow", () => {   
+        cy.get('.m-pagination_next').eq(1).click();
         cy.get('.m-pagination_input').should('have.value', 2);
         cy.get('.m-pagination_prev').eq(1).click();
         cy.get('.m-pagination_input').should('have.value', 1);
-        cy.get('.m-pagination_input').clear().type('2').click();
-        cy.get('.m-pagination_input').should('have.value', 2);
+    });
+
+    it("Check input pagination field", () => {      
+        cy.get('.m-pagination_input').clear().type('2{enter}');
+        cy.get('.js-jumpToPage').should('have.value', 2);
     });
 });
